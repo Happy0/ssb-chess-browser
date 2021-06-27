335bae6a6deaa9edd430b62513432edfc6cf3477
Anders Rune Jensen <arj03@protonmail.ch>
2021-06-27 21:16:32 +0200

    Make it do what it promised to do

--- SsbClient.js
+++ SsbClient.js
@@ -10,45 +10,54 @@ function extraModules(secretStack) {
 }
 
 module.exports = function getClient() {
-    const config = {
-        caps: { shs: Buffer.from(getCaps(), 'base64') },
-        friends: {
-          hops: 1,
-          hookReplicate: false
-        },
-        connections: {
-            incoming: {
-              tunnel: [{ scope: 'public', transform: 'shs' }]
-            },
-            outgoing: {
-              net: [{ transform: 'shs' }],
-              ws: [{ transform: 'shs' }, { transform: 'noauth' }],
-              tunnel: [{ transform: 'shs' }]
-            }
-          },
-        hops: 1,
-        conn: {
-          autostart: false,
-          hops: 1,
-          populatePubs: false
-        }
-      };
+  const config = {
+    caps: { shs: Buffer.from(getCaps(), 'base64') },
+    friends: {
+      hops: 1,
+      hookReplicate: false
+    },
+    connections: {
+      incoming: {
+        tunnel: [{ scope: 'public', transform: 'shs' }]
+      },
+      outgoing: {
+        net: [{ transform: 'shs' }],
+        ws: [{ transform: 'shs' }, { transform: 'noauth' }],
+        tunnel: [{ transform: 'shs' }]
+      }
+    },
+    hops: 1,
+    conn: {
+      autostart: false,
+      hops: 1,
+      populatePubs: false
+    }
+  };
 
-    console.log("Promising hings")
-    return new Promise((resolve, reject) => {
-        ssbSingleton.init(config, extraModules, () => {
-            console.log("giving back the hingmy that I promised")
-            resolve(window.singletonSSB)
+  console.log("Promising hings")
+  const p = new Promise((resolve, reject) => {
+    ssbSingleton.init(config, extraModules, () => {
+      console.log("giving back the hingmy that I promised")
+      resolve(window.singletonSSB)
 
-            // ssbSingleton.getSSBEventually(-1, () => true, /** ??? **/ () => true /*???*/,  (err, sbot) => {
-            //     if (err) {
-            //         reject(err);
-            //     } else {
-            //         resolve(sbot);
-            //     }
-            // })
-        })
+      // ssbSingleton.getSSBEventually(-1, () => true, /** ??? **/ () => true /*???*/,  (err, sbot) => {
+      //     if (err) {
+      //         reject(err);
+      //     } else {
+      //         resolve(sbot);
+      //     }
+      // })
     })
-    
+  })
 
+  ssbSingleton.getSSBEventually(
+    -1,
+    () => { return true },
+    (SSB) => { return SSB },
+    (err, SSB) => {
+      console.log("got SSB")
+    }
+  )
+
+  return p
 }
